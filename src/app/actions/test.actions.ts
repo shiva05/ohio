d682e0026af3dc7acc2913e93dd9bc989e618c67
unknown <maharshi.nandibhatla@trinet.com>
2019-07-09 18:33:14 -0700

    state management implementation

---
+++ src/app/actions/test.actions.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core'
+import { Action } from '@ngrx/store'
+import { Test } from './../models/test.model'
+
+export const ADD_TEST       = '[TEST] Add'
+export const REMOVE_TEST    = '[TEST] Remove'
+
+export class AddTest implements Action {
+  readonly type = ADD_TEST
+
+  constructor(public payload: Test) {}
+}
+
+export class RemoveTest implements Action {
+  readonly type = REMOVE_TEST
+
+  constructor(public payload: number) {}
+}
+
+export type Actions = AddTest | RemoveTest
--- src/app/components/haeder/header.component.ts
+++ src/app/components/haeder/header.component.ts
@@ -1,5 +1,11 @@
 import { Component, EventEmitter, Output, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+import  {Store } from '@ngrx/store';
+import  {Test} from './../../models/test.model';
+import { AppState } from './../../app.state';
+import * as TestActions  from './../../actions/test.actions';
+
 
 @Component({
   selector: 'header',
@@ -9,6 +15,7 @@ import { FormGroup } from '@angular/forms';
 
 export class HeaderComponent {
 
+   tests :Observable<Test[]>;
   @Output() onPageSelect = new EventEmitter<any>();
   ngOnInit() {
 
@@ -16,5 +23,12 @@ export class HeaderComponent {
   goToPage(org){
     this.onPageSelect.emit(org);
   }
+ constructor(private store:Store<AppState>){
+    this.tests = store.select('test');
+ }
+ addTutorial(name, url) {
+
+  this.store.dispatch(new TestActions.AddTest({name :'Test State',url:'google.com'}));
+}
 
 }
