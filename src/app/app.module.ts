7df404bcf673e9eb7ffd6a13ae3800188f8e1c60
UnsungGhost <randypweyer@gmail.com>
2019-07-10 11:09:49 -0700

    Add http module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,17 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import {HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './components/header/header.component';
 import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
 import { AdvancedSearchComponent} from './components/advanced-search/advanced-search.component';
 import { CustomAccordionComponent } from './components/custom-accordion/custom-accordion.component';
 import { FilterSummaryComponent } from './components/filter-summary/filter-summary.component';
 import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools'
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 // import { reducers, metaReducers } from './reducers';
 import { EffectsModule } from '@ngrx/effects';
 import { AppEffects } from './app.effects';
-import {testReducer } from './reducers/test.reducer';
+import { testReducer } from './reducers/test.reducer';
 
 
 
@@ -34,7 +34,8 @@ import {testReducer } from './reducers/test.reducer';
     ReactiveFormsModule,
     // StoreModule.forRoot(reducers, { metaReducers }),
     StoreModule.forRoot({test: testReducer}),
-    StoreDevtoolsModule.instrument({maxAge:100,name:'tng1'}),
+    StoreDevtoolsModule.instrument({maxAge: 100, name: 'tng1'}),
+    HttpClientModule,
     EffectsModule.forRoot([AppEffects])
 
   ],
