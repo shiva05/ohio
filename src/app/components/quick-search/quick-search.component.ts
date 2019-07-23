78265c441b120891b294b7d3be0194f5fbad33f4
unknown <maharshi.nandibhatla@trinet.com>
2019-07-23 15:05:55 -0700

    quik search component changes

---
+++ src/app/components/quick-search/quick-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quick-search',
+  templateUrl: './quick-search.component.html',
+  styleUrls: ['./quick-search.component.css']
+})
+export class QuickSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
