2207ba1b57da3de8b36c45757b47f6f64d8684f5
UnsungGhost <randypweyer@gmail.com>
2019-07-29 14:48:52 -0700

    Add file uploading and file type verification

--- src/app/components/export/export.component.ts
+++ src/app/components/export/export.component.ts
@@ -6,15 +6,47 @@ import { Component, EventEmitter, Output, OnInit } from '@angular/core';
   styleUrls: ['./export.component.css']
 })
 export class ExportComponent implements OnInit {
+  dropdownSettings: any = {};
 
   constructor() { }
 
   @Output() onPageSelect = new EventEmitter<any>();
 
   ngOnInit() {
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: 'item_id', textField: 'item_text',
+      selectAllText: 'Select All',
+      unSelectAllText: 'Unselect All',
+      itemsShowLimit: 1,
+      allowSearchFilter: true
+    };
   }
 
   goToPage(org) {
     this.onPageSelect.emit(org);
+  }
+
+  checkfile(uploadedFile) {
+    const validExts = new Array('.xlsx', '.xls');
+    let fileExt = uploadedFile.value;
+    fileExt = fileExt.substring(fileExt.lastIndexOf('.'));
+    console.log(fileExt);
+    if (validExts.indexOf(fileExt) < 0) {
+      alert('Invalid file selected, valid files are of ' +
+        validExts.toString() + ' types.');
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  import() {
+    const uploadedFile = document.getElementById('excel-upload') || {};
+    this.checkfile(uploadedFile);
+
+    // TODO: write function
+    console.log(uploadedFile);
+    console.log('Import function ran');
   }
 }
