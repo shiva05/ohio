6b21f17997a4a6d8c4b63535c0709e37f8eea148
Gunjan Kansara <gunjan.kansara@ptgindia.com>
2019-07-31 18:13:41 +0530

    Adding 4th Condition in Custom Accordion

--- src/app/components/custom-accordion/custom-accordion.component.ts
+++ src/app/components/custom-accordion/custom-accordion.component.ts
@@ -8,8 +8,9 @@ import { Component, EventEmitter, Output, Input, OnInit } from '@angular/core';
 
 export class CustomAccordionComponent implements OnInit {
   data: any;
-  childList: any = [];
-  parentChildList: any = [];
+  strands: any = [];
+  competency: any = [];
+  outcomes: any = [];
   careerToAcademic: boolean = true;
   academicToCareer: boolean = false;
 
@@ -20,31 +21,37 @@ export class CustomAccordionComponent implements OnInit {
   ngOnInit() {
     this.data = {};
 
-    this.childList = [
+    this.competency = [
       // tslint:disable-next-line:max-line-length
-      { id: 1, academicSubject: 'Math', value: 'Select materials and lay out rough\u2010in wiring runs according to specifications, drawings and code requirements.' },
-      { id: 2, academicSubject: 'Math', value: 'Lay out and install conduit or cable runs, raceways and cable systems.' }
+      { id: 1, academicSubject: 'Math', value: 'Use economic indicators to identify economic trends and conditions.' },
+      { id: 2, academicSubject: 'Math', value: 'Find surface area and volume for three‐dimensional objects, accurate to a specified level of precision.' }
     ];
 
-    this.parentChildList = [
+    this.outcomes = [
+      // tslint:disable-next-line:max-line-length
+      { id: 1, academicSubject: 'Math', value: 'Select materials and lay out rough\u2010in wiring runs according to specifications, drawings and code requirements.' },
+      { id: 2, academicSubject: 'Math', value: 'Lay out and install conduit or cable runs, raceways and cable systems.', competency: JSON.parse(JSON.stringify(this.competency)) }
+    ];
+
+    this.strands = [
       { id: 1, academicSubject: 'Math', value: 'Planning and Design' },
       { id: 2, academicSubject: 'Math', value: 'Business Operations\/21st Century Skills' },
       { id: 3, academicSubject: 'Math', value: 'Construction and Facility Management' },
-      { id: 4, academicSubject: 'Math', value: 'Electrical', childList: JSON.parse(JSON.stringify(this.childList)) },
+      { id: 4, academicSubject: 'Math', value: 'Electrical', outcomes: JSON.parse(JSON.stringify(this.outcomes)) },
       { id: 5, academicSubject: 'Math', value: 'Environmental Systems and Plumbing' },
       { id: 6, academicSubject: 'Math', value: 'Structural Construction' },
       { id: 7, academicSubject: 'Math', value: 'Safety, Tools, and Equipment' }
     ];
 
     // List object having hierarchy of parents and its children
-    this.data.ParentChildchecklist = [
+    this.data.careerField = [
       { id: 1, academicSubject: 'Science', value: 'Hospitality and Tourism' },
       { id: 2, academicSubject: 'Social', value: 'Business, Marketing, and Finance' },
       { id: 3, academicSubject: 'Science', value: 'Agriculture and Environmental Science' },
       { id: 4, academicSubject: 'Science', value: 'Engineering and Science Technologies' },
       { id: 5, academicSubject: 'Math', value: 'Manufacturing' },
       { id: 6, academicSubject: 'Social', value: 'Education and Training' },
-      { id: 7, academicSubject: 'Math', value: 'Construction', parentChildList: JSON.parse(JSON.stringify(this.parentChildList)) },
+      { id: 7, academicSubject: 'Math', value: 'Construction', strands: JSON.parse(JSON.stringify(this.strands)) },
       { id: 8, academicSubject: 'Math', value: 'Transportation' },
       { id: 9, academicSubject: 'Social', value: 'Human Services' },
       { id: 10, academicSubject: 'Social', value: 'Law & Public Safety' },
@@ -54,60 +61,99 @@ export class CustomAccordionComponent implements OnInit {
     ];
   }
 
-  // Click event on parent checkbox
-  parentCheckBox(parentObj) {
+  // Click event on Career Field
+  careerFieldCheckBox(parentObj) {
     // tslint:disable-next-line:prefer-for-of
-    for (let i = 0; i < parentObj.parentChildList.length; i++) {
-      parentObj.parentChildList[i].isSelected = parentObj.isSelected;
-      if (parentObj.parentChildList[i].childList) {
+    for (let i = 0; i < parentObj.strands.length; i++) {
+      parentObj.strands[i].isSelected = parentObj.isSelected;
+      if (parentObj.strands[i].outcomes) {
         // tslint:disable-next-line:prefer-for-of
-        for (let j = 0; j < parentObj.parentChildList[i].childList.length; j++) {
-          parentObj.parentChildList[i].childList[j].isSelected = parentObj.isSelected;
+        for (let j = 0; j < parentObj.strands[i].outcomes.length; j++) {
+          parentObj.strands[i].outcomes[j].isSelected = parentObj.isSelected;
+
+          if (parentObj.strands[i].outcomes[j].competency) {
+            // tslint:disable-next-line:prefer-for-of
+            for (let k = 0; k < parentObj.strands[i].outcomes[j].competency.length; k++) {
+              parentObj.strands[i].outcomes[j].competency[k].isSelected = parentObj.isSelected;
+            }
+          }
         }
       }
     }
   }
 
-  // Click event on Parent Child Checkbox
-  parentChildCheckBox(parent, parentObj) {
+  // Click event on Strand Checkbox
+  strandCheckBox(parent, parentObj) {
     // tslint:disable-next-line:only-arrow-functions
-    parent.isSelected = parent.parentChildList.every(function (itemChild: any) {
+    parent.isSelected = parent.strands.every(function (itemChild: any) {
       return itemChild.isSelected === true;
     });
 
-    if (parentObj.childList) {
+    if (parentObj.outcomes) {
       if (parentObj.isSelected) {
-        parentObj.childList.forEach(item => {
+        parentObj.outcomes.forEach(item => {
           item.isSelected = true;
         });
       } else {
-        parentObj.childList.forEach(item => {
+        parentObj.outcomes.forEach(item => {
           item.isSelected = false;
         });
       }
     }
   }
 
-  // Click event on Child Checkbox
-  childCheckBox(parent, parentObj) {
+  // Click event on Outcome Checkbox
+  outcomeCheckBox(career, strands) {
     // tslint:disable-next-line:only-arrow-functions
-    parentObj.isSelected = parentObj.childList.every(function (itemChild: any) {
+    strands.isSelected = strands.outcomes.every(function (itemChild: any) {
       return itemChild.isSelected === true;
     });
+
     // tslint:disable-next-line:only-arrow-functions
-    parent.isSelected = parent.parentChildList.every(function (itemChild: any) {
+    career.isSelected = career.strands.every(function (itemChild: any) {
+      return itemChild.isSelected === true;
+    });
+
+    // tslint:disable-next-line:only-arrow-functions
+    career.strands.isSelected = strands.outcomes.every(function (itemChild: any) {
       return itemChild.isSelected === true;
     });
   }
 
-  // Expand/Collapse event on each parent
-  expandCollapse(obj) {
-    obj.isClosed = !obj.isClosed;
+  // Click event on Outcome Checkbox
+  competencyCheckBox(career, strand, outcome) {
+    console.log(career);
+    console.log(strand);
+    console.log(outcome);
+    // tslint:disable-next-line:only-arrow-functions
+    // outcome.isSelected = outcome.competency.every(function (itemChild: any) {
+    //   return itemChild.isSelected === true;
+    // });
+
+    // tslint:disable-next-line:only-arrow-functions
+    // career.isSelected = career.strands.outcomes.every(function (itemChild: any) {
+    //   return itemChild.isSelected === true;
+    // });
+
+    // tslint:disable-next-line:only-arrow-functions
+    // career.strands.isSelected = strand.outcomes.every(function (itemChild: any) {
+    //   return itemChild.isSelected === true;
+    // });
   }
 
-  // Expand/Collapse event on each parent
-  expandChildCollapse(obj) {
-    obj.isChildClosed = !obj.isChildClosed;
+  // Expand/Collapse event on Career Field
+  expandCollapseCareerField(obj) {
+    obj.isCareerFieldClosed = !obj.isCareerFieldClosed;
+  }
+
+  // Expand/Collapse event on Strand
+  expandCollapseStrand(obj) {
+    obj.isStrandClosed = !obj.isStrandClosed;
+  }
+
+  // Expand/Collapse event on Outcome
+  expandCollapseOutcome(obj) {
+    obj.isOutcomeClosed = !obj.isOutcomeClosed;
   }
 
   getCheckedValues(item) {
