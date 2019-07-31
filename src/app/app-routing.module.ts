3e7a606cfabc94789c3b2ebd098440f025c33bc2
UnsungGhost <randypweyer@gmail.com>
2019-07-31 16:08:28 -0700

    add route for search

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { MainComponent } from './components/main/main.component';
 import { LoginComponent } from './components/login/login.component';
 import { MyAppComponent } from './components/my-app/my-app.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { SearchResultsComponent } from './components/search-results/search-results.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'main', component: MainComponent },
+  { path: 'Search', component: SearchResultsComponent },
   { path: 'quickSearch', component: QuickSearchComponent },
   { path: 'login', component: LoginComponent },
   { path: 'myApp', component: MyAppComponent },
