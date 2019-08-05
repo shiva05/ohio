df797747645d73b76646e91f01aada0993af5a94
UnsungGhost <randypweyer@gmail.com>
2019-08-05 15:01:24 -0700

    Set up qa environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
-export const environment = {
-  production: true
+const alignmentSearchAPI = 'http://edu-dev-sbd-alignmentsearch.azurewebsites.net/api/';
+const webServer = 'https://edu-dev-sbd.azurewebsites.net/';
+
+const environment = {
+  production: true,
+  GetAlignmentMetaData: `${alignmentSearchAPI}GetAlignmentMetaData?code=E1ya26DNWM67/EJxQQKsdDR/RtTdW0bt9FIhCptxwUY4i0XUaL9quA==`
 };
+
+export { environment };
