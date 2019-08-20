9794e03246d000ee995313d3b4293885c48a853b
unknown <maharshi.nandibhatla@trinet.com>
2019-08-20 15:12:53 -0700

    environment setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
-const alignmentSearchAPI = 'http://edu-dev-sbd-alignmentsearch.azurewebsites.net/api/';
+const alignmentSearchAPI = 'http://edu-prod-sbd-alignmentsearch.azurewebsites.net/api/';
 const webServer = 'https://edu-dev-sbd.azurewebsites.net/';
 
 const environment = {
   production: true,
+  name :"(prod)",
   GetAlignmentMetaData: `${alignmentSearchAPI}GetAlignmentMetaData?code=E1ya26DNWM67/EJxQQKsdDR/RtTdW0bt9FIhCptxwUY4i0XUaL9quA==`,
-  GetCompetencyData: `${alignmentSearchAPI}GetCTECompetencyMetadata?code=VUcOyaCmFOHX7GJ6S8jmllLiZdue3nsCAoFS3gUSCR0TnHBVaNFxhA==`
-
+  GetCompetencyData: `${alignmentSearchAPI}GetCTECompetencyMetadata?code=VUcOyaCmFOHX7GJ6S8jmllLiZdue3nsCAoFS3gUSCR0TnHBVaNFxhA==`,
+  GetCourseMetaData: `${alignmentSearchAPI}GetCourseSearchMetadata?code=7xai9trSe6C5gfgUYdj7U4nTS3CkahLTtWDWmQDyuoNHExjYVxRipg==`
 };
 
 export { environment };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ const alignmentSearchAPI = 'http://edu-dev-sbd-alignmentsearch.azurewebsites.net
 const webServer = 'https://edu-dev-sbd.azurewebsites.net/';
 
 const environment = {
-  production: true,
+  production: false,
+  name :"(DEV)",
   GetAlignmentMetaData: `${alignmentSearchAPI}GetAlignmentMetaData?code=E1ya26DNWM67/EJxQQKsdDR/RtTdW0bt9FIhCptxwUY4i0XUaL9quA==`,
   GetCourseMetaData: `${alignmentSearchAPI}GetCourseSearchMetadata?code=7xai9trSe6C5gfgUYdj7U4nTS3CkahLTtWDWmQDyuoNHExjYVxRipg==`,
   GetCompetencyData: `${alignmentSearchAPI}GetCTECompetencyMetadata?code=VUcOyaCmFOHX7GJ6S8jmllLiZdue3nsCAoFS3gUSCR0TnHBVaNFxhA==`
