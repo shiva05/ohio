2a9da4df5204b89d99a09e16e44834db27085245
Maharshi Harsha <34265356@id.ohio.gov>
2019-09-06 16:07:12 -0700

    url changes

--- src/environments/api.ts
+++ src/environments/api.ts
@@ -3,8 +3,8 @@ export const BASE_HREF = '/case';
 
 export const api = function (serverUrl) {
     return {
-      GetAlignmentMetaData: `${serverUrl}GetAlignmentMetaData?code=E1ya26DNWM67/EJxQQKsdDR/RtTdW0bt9FIhCptxwUY4i0XUaL9quA==`,
-      GetCompetencyData: `${serverUrl}GetCTECompetencyMetadata?code=VUcOyaCmFOHX7GJ6S8jmllLiZdue3nsCAoFS3gUSCR0TnHBVaNFxhA==`,
+      GetAlignmentMetaData: `${serverUrl}AlignmnetSearch/alignmentsMetadata`,
+      GetCompetencyData: `${serverUrl}AlignmnetSearch/cteCompetencyMetadata`,
       GetSearchResultData: `${serverUrl}AlignmnetSearch/SearchAlignments`,
       GetReportData: `${serverUrl}AlignmnetSearch/searchReport`,
       GetDownloadedReport: `${serverUrl}AlignmnetSearch/downloadPdfReport`,
