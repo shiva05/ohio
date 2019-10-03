6ac842777041402815d8d5164d0161187ff95b79
Shiva Krishna <koppolu.shivakrishna@gmail.com>
2019-10-03 11:46:59 +0530

    alignment toggle multiple data binding issue and Unwanted code removed in files.

--- src/app/components/alignment-search-filters/alignment-search-filters.ts
+++ src/app/components/alignment-search-filters/alignment-search-filters.ts
@@ -15,9 +15,6 @@ import { SharedService } from '../../services/shared.service';
 })
 
 export class AlignmentSearchFiltersComponent implements OnInit {
-  disabled = false;
-  ShowFilter = true;
-  limitSelection = false;
   careers: any = [];
   academicSubjects: any = [];
   strands: any = [];
@@ -28,7 +25,6 @@ export class AlignmentSearchFiltersComponent implements OnInit {
   strandsDropdown: any = [];
   outcomesDropdown: any = [];
   selectedKeyword: any;
-  selectedItems: any = [];
   selectedCareer: any = [];
   selectedAcadamicSubjects: any = [];
   selectedStrands: any = [];
@@ -158,16 +154,12 @@ export class AlignmentSearchFiltersComponent implements OnInit {
     }
   }
 
-  //isEmptyObject(obj) {
-  //  return (obj && (Object.keys(obj).length === 0));
-  //}
 
   onCareerSelect() {
     this.strandsDropdown = [];
     var strandsId = [];
     var selectedStrandId = [];
     var finalUpdatedSelectListIds = [];
-   // this.selectedStrands = [];
     this.strands.forEach(eachStrand => {
       this.selectedCareer.forEach(eachCareer => {
         if (eachStrand.CareerFieldPk === eachCareer.CareerFieldId) {
@@ -201,9 +193,6 @@ export class AlignmentSearchFiltersComponent implements OnInit {
       });
     }
   }
-
-
-
 
   onStrandSelect() {
     var outcomesId = [];
@@ -291,9 +280,6 @@ export class AlignmentSearchFiltersComponent implements OnInit {
         }
       });
     });
-  }
-
-  sendSub(item) {
   }
 
   onSubjectLevelsSelect(data) {
@@ -412,12 +398,5 @@ export class AlignmentSearchFiltersComponent implements OnInit {
       this.rout.navigate(['/AlignmentSearchResults']);
       this.store.dispatch({ type: AdvancedSearchActions.SAVE_AS_SELECTED_FILTERS, payload: this.searchObj });
     }
-  }
-
-  onAcadamicSubjectSelect() {
-  }
-
-  goToPage(org) {
-    this.onPageSelect.emit(org);
   }
 }
