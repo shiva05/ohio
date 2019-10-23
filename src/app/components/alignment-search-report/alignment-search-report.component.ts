41602e2a56cb5790efe8f14f7183f30bf4bba0c9
James Rory Hanley <hanley.doth@outlook.com>
2019-10-22 17:35:07 -0700

    Fixed routing

--- src/app/components/alignment-search-report/alignment-search-report.component.ts
+++ src/app/components/alignment-search-report/alignment-search-report.component.ts
@@ -38,7 +38,7 @@ export class AlignmentSearchReportComponent implements OnInit {
       event.returnValue = '';
     });
     if(this.browserRefresh == true){
-        this.rout.navigate(['/Home']);
+        this.rout.navigate(['']);
     };
     this.store.select('authState').subscribe((authState) => {
       if (authState != null) {
