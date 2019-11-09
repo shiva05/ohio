4c784a3fcf152102dc67b0ab0750ffb4a8476ce2
Maharshi Harsha <34265356@id.ohio.gov>
2019-11-08 21:40:20 -0500

    bug fixs

--- src/app/components/export/export.component.ts
+++ src/app/components/export/export.component.ts
@@ -15,6 +15,12 @@ export class ExportComponent implements OnInit {
   dropdownSettings: any = {};
   uploadedFile: any;
   fileMissing = false;
+  file1ToUpload: File = null;
+  file2ToUpload: File = null;
+  file3ToUpload: File = null;
+  file4ToUpload: File = null;
+  file5ToUpload: File = null;
+  file6ToUpload: File = null;
   fileObject = {
     'DataFileTypeId': 0,
     'DataFileName': '',
@@ -49,59 +55,76 @@ export class ExportComponent implements OnInit {
     });
   }
 
-  getFileUploaded(event, id) {
-    this.fileObject = {
-      'DataFileTypeId': 0,
-      'DataFileName': '',
-      'File': []
-    };
-    var duplicate = [];
-    duplicate = _.where(this.fileList, { DataFileTypeId: id });
-    this.fileObject.DataFileTypeId = id;
-    this.fileObject.DataFileName = event.target.files[0].name;
-    this.fileObject.File = event.target.files[0];
+  getFileUploaded(files: FileList,id) {
+
+    // this.fileObject = {
+    //   'DataFileTypeId': 0,
+    //   'DataFileName': '',
+    //   'File': []
+    // };
+    // var duplicate = [];
+    // duplicate = _.where(this.fileList, { DataFileTypeId: id });
+    // this.fileObject.DataFileTypeId = id;
+    // this.fileObject.DataFileName = event.target.files[0].name;
+    // this.fileObject.File = event.target.files[0];
 
     switch (id){
       case 1184:
-        this.ELA_uploadedFileName = event.target.files[0].name;
+        this.file1ToUpload = files.item(0);
+        this.file1ToUpload['id'] =id;
+        this.ELA_uploadedFileName = this.file1ToUpload.name;
         break;
       case 1185:
-        this.Math_uploadedFileName = event.target.files[0].name;
+      this.file2ToUpload = files.item(0);
+      this.file2ToUpload['id'] =id;
+      this.Math_uploadedFileName = this.file2ToUpload .name;
         break;
-      case 1186:
-        this.Science_uploadedFileName = event.target.files[0].name;
+        case 1186:
+        this.file3ToUpload = files.item(0);
+        this.file3ToUpload['id'] =id;
+        this.Science_uploadedFileName = this.file3ToUpload .name;
         break;
       case 1187:
-        this.Social_uploadedFileName = event.target.files[0].name;
+      this.file4ToUpload = files.item(0);
+      this.file4ToUpload['id'] =id;
+      this.Social_uploadedFileName = this.file4ToUpload .name;
         break;
       case 1188:
-        this.Course_uploadedFileName = event.target.files[0].name;
+      this.file5ToUpload = files.item(0);
+      this.file5ToUpload['id'] =id;
+      this.Course_uploadedFileName = this.file5ToUpload.name;
         break;
       case 1189:
-        this.General_uploadedFileName = event.target.files[0].name;
+      this.file6ToUpload = files.item(0);
+      this.file6ToUpload['id'] =id;
+      this.General_uploadedFileName =this.file6ToUpload .name;
         break;
-    }   
-
-    if (duplicate.length > 0) {
-      this.fileList.forEach(element => {
-        if (element.DataFileTypeId == id) {
-          element.DataFileName = event.target.files[0].name;
-          element.File = event.target.files[0];
-        }
-      });
-    } else {
-      this.fileList.push(this.fileObject);
     }
+
+    // if (duplicate.length > 0) {
+    //   this.fileList.forEach(element => {
+    //     if (element.DataFileTypeId == id) {
+    //       element.DataFileName = event.target.files[0].name;
+    //       element.File = event.target.files[0];
+    //     }
+    //   });
+    // } else {
+    //   this.fileList.push(this.fileObject);
+    // }
   // console.log(this.fileList);
   }
 
   submitUploadedFiles() {
-    this.fileMissing = true;
-    if (this.fileList.length == 6) {
-      this.uploadFileService.SubmitFiles(this.fileList, this.utilsContext).subscribe(res => {
-        console.log('success');
-      });
-    }
+    var filearray =[this.file1ToUpload,this.file2ToUpload,this.file3ToUpload,this.file4ToUpload,this.file5ToUpload,this.file6ToUpload]
+    this.uploadFileService.SubmitFiles(filearray).subscribe(res => {
+           console.log('success');
+       });
+   // this.fileMissing = true;
+    // if (this.fileList.length == 6) {
+    //   this.uploadFileService.SubmitFiles(this.fileList, this.utilsContext).subscribe(res => {
+    //     console.log('success');
+    //   });
+    // }
    // console.log('submited');
   }
 
--- src/app/services/upload-file.service.ts
+++ src/app/services/upload-file.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AppHttpService } from './app-http.service';
 import { environment } from '../../environments/environment';
 import { UtilsContext } from '../models/utils-context';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,14 @@ export class UploadFileService {
 
   constructor(private http: AppHttpService) { }
 
-  SubmitFiles(data, utilsContext) {
-    return this.http.post(environment.uploadFiles + '?moduleKey=' + utilsContext.moduleKey + '&assetTemplateKey=' + utilsContext.assetTemplateKey + '&detailKey=' +
-      utilsContext.detailKey, data);
+  SubmitFiles(data ) : Observable<object> {
+    const formData: FormData = new FormData();
+    formData.append(data[0].id, data[0],  data[0].name);
+    formData.append(data[1].id, data[1], data[1].name);
+    formData.append(data[2].id, data[2], data[2].name);
+    formData.append(data[3].id, data[3], data[3].name);
+    formData.append(data[4].id, data[4], data[4].name);
+    formData.append(data[5].id, data[5], data[5].name);
+    return this.http.post(environment.uploadFiles + '?moduleKey=32&assetTemplateKey=242710&detailKey=1',formData ,);
   }
 }
